9272db5853d7711f4201c95db246bb86baf93651
nosferatuzod86 <adairwolff86@yahoo.com.br>
2023-11-15 17:52:34 -0300

    primeiro commit

---
+++ regradetresporcentagem2.js
@@ -0,0 +1,6 @@
+var valortotal = 1200;
+var porcentagem= 5;
+//var valorentrada = 400
+var valorfinal = ((porcentagem * valortotal)/100)
+
+console.log(`${porcentagem} por cento de ${valortotal} reais é igual à ${valorfinal} reais.`)
